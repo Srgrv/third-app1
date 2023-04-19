50e304554efca2b202de1e82f42ce3a9e3abc493
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-19 19:26:15 +0300

    add PageReduxForm

--- src/components/Content/MyPage/MyPageContainer/Page/Page.jsx
+++ src/components/Content/MyPage/MyPageContainer/Page/Page.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import classes from "./Page.module.css";
 import Status from "./Status/Status";
+import PageReduxForm from "../../../../forms/PageForm/PageReduxForm";
 
 const Page = (props) => {
   const posts = props.posts.map((item, index) => {
     return (
       <div className={classes.posts} key={index}>
         <div>
-          <img src={props.avatar} alt="" />
+          <img src={props.avatar || "../../../../../../logo192.png"} alt="" />
         </div>
         <div>{item}</div>
       </div>
     );
   });
 
+  const hello = (value) => {
+    console.log(value.post);
+    props.add(value.post);
+  };
+
   return (
     <div className={classes.page}>
       <Status status={props.status} updateStatus={props.updateStatus} />
       <div>{props.aboutMe}</div>
       <div>
-        <img src={props.photos} alt="" />
+        <img src={props.photos || "../../../../../../logo192.png"} alt="" />
       </div>
-      <div>
+      <PageReduxForm {...props} onSubmit={hello} />
+      {/* <div>
         <input
           value={props.inputValue}
           onChange={(e) => props.change(e.target.value)}
         />
         <button onClick={props.add}>add</button>
-      </div>
+      </div> */}
       <div>{posts} </div>
     </div>
   );
